abfc91fa55d1da28fbc830d9f54b6abf1ef49ce8
donatir <donatir@lafayette.edu>
2019-05-09 20:06:05 -0400

    Added a GUI with tkinter

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # course_registration
-A quick python program that I made that uses automated keyboard events to speed up class registration.
+
+
+
+****************** ONLY WORKS FOR MAC *****************
+
+
+DESCRIPTION:
+
+A quick python program that I made, which uses automated keyboard events to speed up class registration.
+
+
+
+
+
+
+HOW TO USE:
+
+index.html is setup like the actual registration website so that the user can practice. This ensures that there is no confusion when registration actually occurs.
+
+1. Build register.py and enter your CRN numbers.
+2. Make sure that the registration window is open in the background (such that if you press cmd + tab, it should open)
+3. Press "Submit" and quickly click on the first field.
+4. Your CRN numbers should be filled out shortly–now just click submit
--- register.py
+++ register.py
@@ -1,34 +1,66 @@
 import time
 from pynput.keyboard import Key, Controller
+from tkinter import *
 
-keyboard = Controller()
+num_of_courses = 6
 
-keyboard.press(Key.cmd)
-keyboard.press(Key.tab)
-keyboard.release(Key.cmd)
-keyboard.release(Key.tab)
+def print_data():
 
+	crns = []
+	for i in range(num_of_courses):
+		crns.append(entries[i].get())
 
-time.sleep(1.5)
+	for i in range(num_of_courses):
+		print("{}: {}".format(i + 1, entries[i].get()))
 
-keyboard.type("10200") #CS202
-keyboard.press(Key.tab)
-keyboard.release(Key.tab)
+def submit():
 
-keyboard.type("10201") #CS203
-keyboard.press(Key.tab)
-keyboard.release(Key.tab)
+	crns = []
+	for i in range(num_of_courses):
+		crns.append(entries[i].get())
 
-keyboard.type("10203") #CS203L
-keyboard.press(Key.tab)
-keyboard.release(Key.tab) 
-keyboard.type("10501") #MATH272 (Linear Algebra w/ Application)
-keyboard.press(Key.tab)
-keyboard.release(Key.tab)
+	keyboard = Controller()
 
-keyboard.type("10298") #ENG247 (Nature Writing)
-keyboard.press(Key.tab)
-keyboard.release(Key.tab)
+	keyboard.press(Key.cmd)
+	keyboard.press(Key.tab)
+	keyboard.release(Key.cmd)
+	keyboard.release(Key.tab)
 
-keyboard.press(Key.enter)
-keyboard.release(Key.enter)
+	time.sleep(1.5)
+
+	for i in range(num_of_courses):
+		keyboard.type(crns[i])
+		keyboard.press(Key.tab)
+		keyboard.release(Key.tab)
+
+def select_all(event):
+	event.widget.selection_range(0, END)
+
+w = Tk()
+w.title("Course Registration Helper")
+w.resizable(height = False, width = False)
+#Creates labels
+labels = []
+for i in range(num_of_courses):
+	labels.append(Label(w, text = "{}. ".format(i + 1)))
+
+#Creates input
+entries = []
+for i in range(num_of_courses):
+	entries.append(Entry(w))
+
+submit = Button(w, text = "Submit", command = submit)
+
+#Shows labels
+for i, label in enumerate(labels):
+	label.grid(row = i, column = 0)
+
+#Shows input
+for i, entry in enumerate(entries):
+	entry.grid(row = i, column = 1)
+	entry.insert(0, "CRN {}".format(i + 1))
+	entry.bind("<FocusIn>", select_all)
+
+submit.grid(row = num_of_courses, column = 0)
+
+w.mainloop()
